c5a8302b4bc6b4ee926714e454afc1b7104494f2
ogapants <ogapamyu@gmail.com>
2018-06-17 23:55:37 +0900

    add func

--- app/src/main/java/com/github/ogapants/dagger2sample/MainActivity.kt
+++ app/src/main/java/com/github/ogapants/dagger2sample/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.github.ogapants.dagger2sample
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.button).setOnClickListener {
+            startActivity(Intent(this@MainActivity, MainActivity::class.java))
+        }
     }
 }
